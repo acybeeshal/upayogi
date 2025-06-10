461dfff6cf075aff8a9a506c8ee654065e8e0c09
acybeeshal <acybeeshal@gmail.com>
2025-06-11 00:20:10 +0545

    added our team panel

--- frontend upayogi/js/main.js
+++ frontend upayogi/js/main.js
@@ -1,3 +1,5 @@
+ 
+ //for the nav bar to make fix and close the dropdown button after the click
  document.addEventListener("DOMContentLoaded", function () {
     var navLinks = document.querySelectorAll('.navbar-nav .nav-link');
     var navbarCollapse = document.getElementById('navbarNav');
@@ -11,3 +13,14 @@
       });
     });
   });
+
+  //for the our team to make the 
+    const container = document.getElementById("teamScroll");
+
+  function scrollLeft() {
+    container.scrollBy({ left: -300, behavior: "smooth" });
+  }
+
+  function scrollRight() {
+    container.scrollBy({ left: 300, behavior: "smooth" });
+  }
